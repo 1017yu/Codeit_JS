145b2b89dd515b776b6d5c7864df27e99fa41654
1017yu <1017yu@daum.net>
2021-06-16 11:09:38 +0900

    Add strnum.html

---
+++ strnum.js
@@ -0,0 +1,16 @@
+// 숫자형과 문자열 재료
+let material1 = 3;
+let material2 = '3';
+let material3 = 10;
+let material4 = '4';
+let material5 = 4;
+
+// 연산 결과
+let result1 = material2 + material4;  // '3' + '4' = '34'
+let result2 = material1 * material3 + material5;  // 3 * 10 + 4 = 34
+
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
