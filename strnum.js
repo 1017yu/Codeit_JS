8f5a40f2b35ca40d7449cd7d50427d02819d4654
1017yu <1017yu@daum.net>
2021-06-16 11:11:09 +0900

    Update strnum.js

--- strnum.js
+++ strnum.js
@@ -10,7 +10,7 @@ let result1 = material2 + material4;  // '3' + '4' = '34'
 let result2 = material1 * material3 + material5;  // 3 * 10 + 4 = 34
 
 // 테스트 코드
-console.log(result1);
-console.log(typeof result1);
-console.log(result2);
-console.log(typeof result2);
+console.log(result1);  // 34
+console.log(typeof result1);  // string
+console.log(result2);  // 34
+console.log(typeof result2);  // number
